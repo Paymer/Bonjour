3f5db383be198f26e4b23f3d2ef4c60cc0818fcf
Paula Aymerich <p.aymerich.f@gmail.com>
2017-07-04 10:52:25 +0200

    Commit 1

---
+++ Bonjour.java
@@ -0,0 +1,30 @@
+/** 
+*@author Paupau
+*@see www.XYZ.com 
+*@version 1.0
+*/
+
+
+public class Bonjour{
+
+
+
+    public static void main(String args[]){
+
+        //Para compilar utilizar comando "javac Bonjour.java"
+        /* Para lanzar el programa utilizar "java Bonjour (argumentos a anyadir)"*/
+        /** Que divertido es aprender java! 
+        *Antes de compilar modifica la variable PATH e indicale donde esta el directorio .bin del JDK */
+
+        /**
+        *Esto es lo que se genera cuando haces "javadoc Bonjour.java"
+         */
+
+        System.out.println("Bonjour tout le monde" +args[0]);
+
+        //Detalles a mitad de codigo
+        /* Detalles tecnicos*/
+        /** Documentacion tecnica, sitaxis... */
+
+    }
+}
